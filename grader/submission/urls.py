332109486f5666009b5e5073b191d9db98063f54
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-06-12 00:22:22 +0700

    Allow multi character test id

--- grader/submission/urls.py
+++ grader/submission/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from .views import *
 
 urlpatterns = [
-	url(r'^test/(?P<id>[0-9])/stats', Stats.as_view()),
-	url(r'^test/(?P<id>[0-9])/scoreboard', Scoreboard.as_view()),
+	url(r'^test/(?P<id>[0-9]+)/stats', Stats.as_view()),
+	url(r'^test/(?P<id>[0-9]+)/scoreboard', Scoreboard.as_view()),
 ]
