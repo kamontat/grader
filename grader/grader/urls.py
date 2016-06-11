7dafc0b35623953d0beacb9bb0616a232dc71449
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-06-11 22:25:38 +0700

    Use /server/ as root

--- grader/grader/urls.py
+++ grader/grader/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('authapi.urls')),
-    url(r'', include('problems.urls'))
+    url(r'^server/admin/', admin.site.urls),
+    url(r'server/', include('authapi.urls')),
+    url(r'server/', include('problems.urls'))
 ]
