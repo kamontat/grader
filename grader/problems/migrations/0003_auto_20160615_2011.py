ddc96dfdad56ef69288a7720cda9f7c60471e395
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-06-15 20:49:13 +0700

    Use the correct exception for newer python versions

--- grader/problems/migrations/0003_auto_20160615_2011.py
+++ grader/problems/migrations/0003_auto_20160615_2011.py
@@ -13,9 +13,7 @@ def convert_yaml(apps, schema_editor):
         try:
             data = json.loads(problem.graders)
             data = data['grader']
-        except json.decoder.JSONDecodeError:
-            data = {}
-        except KeyError:
+        except (ValueError, KeyError):
             data = {}
         problem.graders = yaml.dump(data, default_flow_style=False)
         problem.save()
